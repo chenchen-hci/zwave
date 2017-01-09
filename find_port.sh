ba9b1d6473e795e451488b7df609916bf782bc5e
Chen Chen <chenc5@andrew.cmu.edu>
2017-01-09 00:31:28 -0500

    almost done! :tada: :ballon:

---
+++ find_port.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "-------------------------------------------"
+echo "Full USB port list:"
+lsusb
+
+echo "-------------------------------------------"
+echo "port path:"
+
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && continue
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && continue
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
